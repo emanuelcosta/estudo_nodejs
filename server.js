cf5835d90af60acf2096db2ba94403a67fc8722a
home <emanuelcosta.es@gmail.com>
2024-12-03 22:55:16 -0300

    teste

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Importando o CORS
-require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Configurando CORS para permitir requisições de origens específicas ou de qualquer origem
+
 app.use(cors({
-  origin: 'http://localhost:8080', // Permite somente o frontend no localhost:8080
+  origin: '*', // Permite todos os domínios
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permite os métodos
   allowedHeaders: ['Content-Type', 'Authorization'] // Permite os cabeçalhos específicos
 }));
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch((err) => console.error('Erro ao conectar com MongoDB:', err));
 
 // Importando as rotas
-const membroRoutes = require('./routes/membroRoutes');
+const membroRoutes = require('./routes/membroRoutes.js');
 
 // Usando as rotas para todas as rotas que começam com /api
 app.use('/api', membroRoutes);
