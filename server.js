f04381f71222b367cce5fe3398bafc53f1d16f05
home <emanuelcosta.es@gmail.com>
2024-12-03 23:50:47 -0300

    teste integração git

--- server.js
+++ server.js
@@ -25,9 +25,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Importando as rotas
 const membroRoutes = require('./routes/membroRoutes.js');
+const pedidoRevistasRoutes = require('./routes/pedidoRevistasRoutes.js');
 
 // Usando as rotas para todas as rotas que começam com /api
 app.use('/api', membroRoutes);
+app.use('/api', pedidoRevistasRoutes);
 
 // Iniciar o servidor
 app.listen(PORT, () => {
